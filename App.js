62dc192b7ac0277ba44c88d2d6ab437d6682faa5
unknown <brayanhdez696@gmail.com>
2026-05-23 03:16:59 -0600

    Se actualizo el proyecto

--- App.js
+++ App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+});
 import {
   View,
   Text,
